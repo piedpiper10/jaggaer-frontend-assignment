e72167a0149ef86f3a81cdbfc779321b7197661b
nikhil <jnikhilreddy2016@gmail.com>
2025-08-28 11:24:04 +0530

    clean up code

--- frontend/src/graphql/apolloClient.js
+++ frontend/src/graphql/apolloClient.js
@@ -1,12 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-// Create a link to your GraphQL server
-const link = new HttpLink({
-  uri: "http://127.0.0.1:3000/graphql",
-});
-
-// Initialize Apollo Client
 export const client = new ApolloClient({
-  link,
+  uri: "http://127.0.0.1:3000/graphql",
   cache: new InMemoryCache(),
 });
